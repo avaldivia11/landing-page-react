e80d33636697f77302081b2f892291594c319576
Andres Valdivia <valdiviapericao.av@gmail.com>
2021-11-03 19:23:01 +0000

    navbar y footer

---
+++ src/js/component/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = props => {
+	return (
+		<footer className="bg-dark text-center text-white fixed-bottom">
+			<div className="row">
+				<div>
+					<p>Copyright &copy; Your Website Name 2019</p>
+				</div>
+			</div>
+		</footer>
+	);
+};
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-
+/* import Navbar from "./Navbar" */
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 
 //create your first component
 const Home = () => {
 	return (
-		
+		<>
+			<Navbar />
+			<div className="container-fluid p-0">
+				<div className="row"></div>
+				<div className="row">
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+			<Footer />
+		</>
 	);
 };
 
