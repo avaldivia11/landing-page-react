d332be91b22cbd103d41b395461eec3a5b7c64d7
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-11-03 19:24:18 +0000

    jumbotron mas card

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,30 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import { Jumbotron } from "./jumbotrom";
+import { Card } from "./card";
 //create your first component
 const Home = () => {
 	return (
-		
+		<>
+			<div className="container">
+				<Jumbotron />
+				<div className="row">
+					<div className="col-3">
+						<Card />
+					</div>
+					<div className="col-3">
+						<Card />
+					</div>
+					<div className="col-3">
+						<Card />
+					</div>
+					<div className="col-3">
+						<Card />
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
